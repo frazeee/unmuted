80cfdfe8cc711c3cc41b1e51b0e9efec2a3d9245
Kyle Uy <kyleeeee09@gmail.com>
2024-03-05 18:28:47 +0800

    Updated Page Three, added Page four

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 import "./App.css";
 import TitlePage from "./Pages/titlePage";
 import AgeConsent from "./Pages/ageConsent/ageConsent";
+import PageThree from "./Pages/PageThree/pageThree";
 
 import { useState } from "react";
 
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import { ThemeProvider } from "@emotion/react";
-import PageThree from "./Pages/PageThree/pageThree";
+
+import TypewriterTest from "./Pages/Typewriter";
 
 function App() {
-
-  
   // Create styles using makeStyles
   const theme = createTheme((theme) => ({
     typo: {
       flexGrow: 1,
-      textAlign: "center"
-    }
+      textAlign: "center",
+    },
   }));
-  
- responsiveFontSizes(theme);
+
+  responsiveFontSizes(theme);
 
   const [page, setPage] = useState(0);
 
   const handleProceed = () => {
-    setPage(prevPage => prevPage + 1); 
+    setPage((prevPage) => prevPage + 1);
   };
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App" style={{}}>
-      {page === 0 && <TitlePage onProceed={handleProceed} />}
-      {page === 1 && <AgeConsent onProceed={handleProceed}/>}
-      {page === 2 && <PageThree onProceed={handleProceed}/>}
+        {page === 0 && <TitlePage onProceed={handleProceed} />}
+        {page === 1 && <AgeConsent onProceed={handleProceed} />}
+        {page === 2 && <PageThree onProceed={handleProceed} />}
 
+        <PageThree />
       </div>
     </ThemeProvider>
   );
--- src/Pages/PageThree/pageThree.js
+++ src/Pages/PageThree/pageThree.js
@@ -1,117 +1,141 @@
 import React, { useState, useEffect } from "react";
-import { Typography, Paper, TextField, Button, Grid } from "@mui/material";
+import {
+  Typography,
+  Paper,
+  TextField,
+  Button,
+  Grid,
+  SvgIcon,
+  Box,
+} from "@mui/material";
 import "./styles.css"; // Import the external CSS file
+import ArrowForwardSharpIcon from "@mui/icons-material/ArrowForwardSharp";
+import Typewriter from "../Typewriter";
 
 const PageThree = () => {
-  const [showFirstTypography, setShowFirstTypography] = useState(true);
-  const [showSecondTypography, setShowSecondTypography] = useState(false);
-  const [showThirdTypography, setShowThirdTypography] = useState(false);
-  const [showPaper, setShowPaper] = useState(false);
+  const [currentTypography, setCurrentTypography] = useState(0);
   const [userName, setUsername] = useState("");
 
   const handleSubmit = (event) => {
-      setShowPaper(false);
-      setShowThirdTypography(true)
+    handleNextTypography();
   };
 
-  useEffect(() => {
-    const firstTimeout = setTimeout(() => {
-      setShowFirstTypography(false);
-      setShowSecondTypography(true);
-      setShowPaper(false);
-    }, 5000); // 5 seconds
-
-    const secondTimeout = setTimeout(() => {
-      setShowSecondTypography(false);
-      setShowPaper(true);
-    }, 10000); // 10 seconds
-
-    return () => {
-      clearTimeout(firstTimeout);
-      clearTimeout(secondTimeout);
-    };
-  }, []);
+  const handleNextTypography = () => {
+    setCurrentTypography(currentTypography + 1);
+  };
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        className={showFirstTypography ? "fade-in" : "fade-out"}
-        style={{ textAlign: "center" }}
+    <>
+      <Box
+        position="absolute"
+        top={0}
+        right={0}
+        p={2}
       >
-        {showFirstTypography && (
-          <Typography variant="h4" sx={{ fontWeight: "bold", color: "white" }}>
-            You are about to follow a young woman to a path of healing.
-          </Typography>
-        )}
-      </div>
-      <div
-        className={showSecondTypography ? "fade-in" : "fade-out"}
-        style={{ textAlign: "center" }}
-      >
-        {showSecondTypography && (
-          <Typography variant="h4" sx={{ fontWeight: "bold", color: "white" }}>
-            Let your decisions be her guide.
-          </Typography>
-        )}
-      </div>
-      {showPaper && (
-        <>
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"
-            style={{ height: "100vh" }}
-          >
-            <Paper
-              sx={{ mt: 48 }}
-              elevation={3}
-              className={showPaper ? "fade-in" : "fade-out"}
-              style={{ width: "50%", padding: "20px", textAlign: "center" }}
-            >
-              <Typography variant="h4" gutterBottom>
-                Here she is.
-              </Typography>
-              <Typography variant="h5" gutterBottom>
-                What's her name?
-              </Typography>
-              <TextField
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                value={userName}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleSubmit}
-                style={{ marginTop: "10px", borderRadius: "15px" }}
-              >
-                Continue
-              </Button>
-            </Paper>
-          </Grid>
-        </>
-      )}
+        <ArrowForwardSharpIcon sx={{ color: "white", width: "50px", height: "50px" }} onClick = {handleNextTypography} />
+      </Box>
 
-<div
-        className={showThirdTypography ? "fade-in" : "fade-out"}
-        style={{ textAlign: "center" }}
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        {showThirdTypography && (
-          <Typography variant="h4" sx={{ fontWeight: "bold", color: "white" }}>
-            {userName}, that's a great name.
-          </Typography>
+        <div
+          className={currentTypography === 0 ? "fade-in" : "fade-out"}
+          style={{ textAlign: "center" }}
+        >
+          {currentTypography === 0 && (
+            <Typography
+              variant="h4"
+              sx={{ fontWeight: "bold", color: "white" }}
+            >
+              You are about to follow a young woman to a path of healing.
+            </Typography>
+          )}
+        </div>
+        <div
+          className={currentTypography === 1 ? "fade-in" : "fade-out"}
+          style={{ textAlign: "center" }}
+        >
+          {currentTypography === 1 && (
+            <Typography
+              variant="h4"
+              sx={{ fontWeight: "bold", color: "white" }}
+            >
+              Let your decisions be her guide.
+            </Typography>
+          )}
+        </div>
+        {currentTypography === 2 && (
+          <>
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+              style={{ height: "100vh" }}
+            >
+              <Paper
+                sx={{ mt: 48 }}
+                elevation={3}
+                className={currentTypography === 2 ? "fade-in" : "fade-out"}
+                style={{ width: "50%", padding: "20px", textAlign: "center" }}
+              >
+                <Typography variant="h4" gutterBottom>
+                  Here she is.
+                </Typography>
+                <Typography variant="h5" gutterBottom>
+                  What's her name?
+                </Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
+                  value={userName}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleSubmit}
+                  style={{ marginTop: "10px", borderRadius: "15px" }}
+                >
+                  Continue
+                </Button>
+              </Paper>
+            </Grid>
+          </>
         )}
+
+        <div
+          className={currentTypography === 3 ? "fade-in" : "fade-out"}
+          style={{ textAlign: "center" }}
+        >
+          {currentTypography === 3 && (
+            <Typography
+              variant="h4"
+              sx={{ fontWeight: "bold", color: "white" }}
+            >
+              {userName}, what a beautiful name!
+            </Typography>
+          )}
+        </div>
+
+        <div style={{ textAlign: "center" }}>
+          {currentTypography === 4 && (
+            <Typewriter text="73% of Filipinos are social media users. Gen Z, aged 18 to 24, is the largest segment." />
+          )}
+        </div>
+
+        <div style={{ textAlign: "center" }}>
+          {currentTypography === 5 && (
+         <Typewriter text={`${userName}, belongs to this demographic.`} />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
